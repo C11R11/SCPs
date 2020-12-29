7c68f151d510cb185d48b37bf1c68d1656bdadc4
Cristian Rodriguez <cristianrodriguezcanto@gmail.com>
2020-12-29 17:50:12 -0300

    file reading, restrictions check

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,6 @@ int main()
 {
     HelloWorld world;
     world.say();
-
-        std::cout << "hello: \n";
-
-
-    const int rows = 200;
-    const int columns = 1000;
-    double cost[columns] = {0};
-    double solution[columns] = {0};
-    double sparse[rows][columns] = {0};
-
-    //Calculation of objective function
-    double objective = 0;
-
-    for(size_t columnIndex = 0; columnIndex < columns; columnIndex++)
-    {
-        objective =+ cost[columnIndex] * solution[columnIndex];   
-    }
-
-    std::cout << "objective: " << objective;
-    
+    std::cout << "hello: \n";
     return 0;
 }
